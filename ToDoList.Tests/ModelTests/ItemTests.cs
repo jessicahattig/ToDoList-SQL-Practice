aaf86cf889c5a083d00000f506abcb6414b6e442
jessicahattig <jessicahattig@gmail.com>
2023-09-24 14:12:15 -0700

    Item.cs, first test, green and red phase.

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -30,74 +30,90 @@ namespace ToDoList.Tests
       DBConfiguration.ConnectionString = Configuration["ConnectionStrings:TestConnection"];
     }
 
-
-    // existing tests here
-
+    //New tests for SQL
     [TestMethod]
-    public void ItemConstructor_CreatesInstanceOfItem_Item()
-    {
-      Item newItem = new Item("test");
-      Assert.AreEqual(typeof(Item), newItem.GetType());
-    }
-
-    [TestMethod]
-    public void GetDescription_ReturnsDescription_String()
+    public void GetAll_ReturnsEmptyListFromDatabase_ItemList()
     {
       //Arrange
-      string description = "Walk the dog.";
-
-      //Act
-      Item newItem = new Item(description);
-      string result = newItem.Description;
-
-      //Assert
-      Assert.AreEqual(description, result);
-    }
-
-    [TestMethod]
-    public void SetDescription_SetDescription_String()
-    {
-      //Arrange
-      string description = "Walk the dog.";
-      Item newItem = new Item(description);
-
-      //Act
-      string updatedDescription = "Do the dishes";
-      newItem.Description = updatedDescription;
-      string result = newItem.Description;
-
-      //Assert
-      Assert.AreEqual(updatedDescription, result);
-    }
-
-    [TestMethod]
-    public void GetAll_ReturnsEmptyList_ItemList()
-    {
-      // Arrange
       List<Item> newList = new List<Item> { };
-
-      // Act
-      List<Item> result = Item.GetAll();
-
-      // Assert
-      CollectionAssert.AreEqual(newList, result);
-    }
-
-    [TestMethod]
-    public void GetAll_ReturnsItems_ItemList()
-    {
-      //Arrange
-      string description01 = "Walk the dog";
-      string description02 = "Wash the dishes";
-      Item newItem1 = new Item(description01);
-      Item newItem2 = new Item(description02);
-      List<Item> newList = new List<Item> { newItem1, newItem2 };
-
+      
       //Act
       List<Item> result = Item.GetAll();
-
+      
       //Assert
       CollectionAssert.AreEqual(newList, result);
-    }
+}
+
   }
 }
+
+    // Previous tests
+
+//     [TestMethod]
+//     public void ItemConstructor_CreatesInstanceOfItem_Item()
+//     {
+//       Item newItem = new Item("test");
+//       Assert.AreEqual(typeof(Item), newItem.GetType());
+//     }
+
+//     [TestMethod]
+//     public void GetDescription_ReturnsDescription_String()
+//     {
+//       //Arrange
+//       string description = "Walk the dog.";
+
+//       //Act
+//       Item newItem = new Item(description);
+//       string result = newItem.Description;
+
+//       //Assert
+//       Assert.AreEqual(description, result);
+//     }
+
+//     [TestMethod]
+//     public void SetDescription_SetDescription_String()
+//     {
+//       //Arrange
+//       string description = "Walk the dog.";
+//       Item newItem = new Item(description);
+
+//       //Act
+//       string updatedDescription = "Do the dishes";
+//       newItem.Description = updatedDescription;
+//       string result = newItem.Description;
+
+//       //Assert
+//       Assert.AreEqual(updatedDescription, result);
+//     }
+
+//     [TestMethod]
+//     public void GetAll_ReturnsEmptyList_ItemList()
+//     {
+//       // Arrange
+//       List<Item> newList = new List<Item> { };
+
+//       // Act
+//       List<Item> result = Item.GetAll();
+
+//       // Assert
+//       CollectionAssert.AreEqual(newList, result);
+//     }
+
+//     [TestMethod]
+//     public void GetAll_ReturnsItems_ItemList()
+//     {
+//       //Arrange
+//       string description01 = "Walk the dog";
+//       string description02 = "Wash the dishes";
+//       Item newItem1 = new Item(description01);
+//       Item newItem2 = new Item(description02);
+//       List<Item> newList = new List<Item> { newItem1, newItem2 };
+
+//       //Act
+//       List<Item> result = Item.GetAll();
+
+//       //Assert
+//       CollectionAssert.AreEqual(newList, result);
+//     }
+//   }
+// }
--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using ToDoList.Models;
+
 
 namespace ToDoList
 {
